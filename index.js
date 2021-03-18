fbe396ef0878c80b9ce42d0c45f0813989c3c643
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-03-18 23:22:55 +0200

    mod for generated content

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ import 'ol/ol.css';
 import FullScreen from 'ol/control/FullScreen';
 import apply from 'ol-mapbox-style';
 import Map from 'ol/Map';
-import featuresStyle from './data/152555966.json';
+import featuresStyle from './data/style.json';
+import GeoJSON from 'ol/format/GeoJSON';
 
 const map = new Map({ target: 'map' });
+window.map = map;
 
-apply( 
+apply(
   map, featuresStyle).then(function (map) {
-    map.addControl(new FullScreen());
-});
+    var features = new GeoJSON({
+      featureProjection: 'EPSG:3857',
+      dataProjection: 'EPSG:4326'
+    }).readFeatures(window.geojson);
+    const layer = 
+    map.getLayers().getArray()[1];
+    layer.getSource().addFeatures(features);
+    const extent = layer.getSource().getExtent();
+    map.getView().fit(extent, {
+      size: map.getSize(),
+      padding: [50,50,50,50],
+      maxZoom: 16
+    });
+  });
 
 
