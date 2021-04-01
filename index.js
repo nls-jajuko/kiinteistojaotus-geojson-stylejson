4af20c58334989de7be6c2f1d3f4cea83a372f03
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-04-01 15:03:51 +0300

    toggle mb layer visibility within ol layer

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import 'ol/ol.css';
 import FullScreen from 'ol/control/FullScreen';
-import apply from 'ol-mapbox-style';
+import apply from './mbox-apply';
 import Map from 'ol/Map';
 import featuresStyle from './data/style.json';
 import GeoJSON from 'ol/format/GeoJSON';
+import { getMboxLayoutProperty , setMboxLayoutProperty , getMboxLayer, getOLLayer } from './mbox-layer.js';
 
 const map = new Map({ target: 'map' });
 window.map = map;
 
 apply(
   map, featuresStyle).then(function (map) {
-    var features = new GeoJSON({
+    const format = new GeoJSON({
       featureProjection: 'EPSG:3857',
       dataProjection: 'EPSG:4326'
-    }).readFeatures(window.geojson);
+      }),
+      features = format.readFeatures(window.geojson),
+      featTunnuspiste = format.readFeature({
+        "type":"Feature",
+        "properties": { 'kiinteistotunnuksenEsitysmuoto': features[0].getProperties()['kiinteistotunnuksenEsitysmuoto']},
+        "geometry":features[0].getProperties()['kiinteistotunnuksenSijainti']
+        })
+        ;
+      features.push(featTunnuspiste);
+        window.featTunnuspiste=featTunnuspiste;
+        window.features = features;
+
     const layer = 
     map.getLayers().getArray()[1];
     layer.getSource().addFeatures(features);
@@ -25,4 +37,8 @@ apply(
     });
   });
 
-
+  window.setMboxLayoutProperty = setMboxLayoutProperty;
+  window.getMboxLayoutProperty = getMboxLayoutProperty;
+  window.getMboxLayer = getMboxLayer;
+  window.getOLLayer = getOLLayer;
+  
