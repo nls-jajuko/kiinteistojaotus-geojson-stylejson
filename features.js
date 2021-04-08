b3bdd1b2dbf33da4bc3dd71092a9ec6718229569
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-04-08 18:51:28 +0300

    batch ogcapif client

---
+++ features.js
@@ -0,0 +1,24 @@
+
+
+export async function features(func, endpoint, feat, pargs) {
+
+    const url = new URL(`${endpoint}/collections/${feat}/items`);
+    url.searchParams.set('limit',10);
+    
+    Object.entries(pargs).forEach((item)=>{
+        url.searchParams.set(...item)
+    });
+
+    const next= [url.toString()];
+
+    while (next.length && next[0]) {
+
+        await fetch(next.pop()).then(r => r.json()).then(json => {
+            if (!func(json) || !json.links) {
+
+                return;
+            }
+            next.push(json.links.filter(l => l.rel === 'next').map(l => l.href).pop());
+        });
+    }
+}
--- index.js
+++ index.js
@@ -4,25 +4,41 @@ import apply from 'ol-mapbox-style';
 import Map from 'ol/Map';
 import featuresStyle from './data/style.json';
 import GeoJSON from 'ol/format/GeoJSON';
+import { features } from './features';
+import { transform } from 'ol/proj';
 
-const map = new Map({ target: 'map' });
+const api = 'https://beta-paikkatieto.maanmittauslaitos.fi/kiinteisto-avoin/features/v1',
+  featType = 'RajamerkinSijaintitiedot';
+
+const map = new Map({ target: 'map' }),
+  format = new GeoJSON({
+    featureProjection: 'EPSG:3857',
+    dataProjection: 'EPSG:4326'
+  });
+
+
 window.map = map;
 
 apply(
   map, featuresStyle).then(function (map) {
-    var features = new GeoJSON({
-      featureProjection: 'EPSG:3857',
-      dataProjection: 'EPSG:4326'
-    }).readFeatures(window.geojson);
-    const layer = 
-    map.getLayers().getArray()[1];
-    layer.getSource().addFeatures(features);
-    const extent = layer.getSource().getExtent();
-    map.getView().fit(extent, {
-      size: map.getSize(),
-      padding: [50,50,50,50],
-      maxZoom: 16
+
+    map.on("moveend", (evt) => {
+      const
+        view = map.getView(),
+        layer = map.getLayers().getArray()[1],
+        source = layer.getSource(),
+        bounds = view.calculateExtent(map.getSize()),
+        lb = transform([bounds[0], bounds[1]], 'EPSG:3857', 'EPSG:4326'),
+        rt = transform([bounds[2], bounds[3]], 'EPSG:3857', 'EPSG:4326'),
+        bbox = [lb[0], lb[1], rt[0], rt[1]];
+
+      features((fc) => {
+        source.addFeatures(format.readFeatures(fc));
+        const extent = source.getExtent();
+        return true;
+      }, api, featType, {
+        limit: 200,
+        bbox: bbox.join(',')
+      });
     });
   });
-
-
