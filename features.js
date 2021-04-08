48d2068509e4cfe34484ccbda673524c439d464c
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-04-08 22:08:04 +0300

    8 lines OGC API Features batching client

--- features.js
+++ features.js
@@ -1,13 +1,8 @@
 export async function features(func, endpoint, feat, params) {
     const url = new URL(`${endpoint}/collections/${feat}/items`);
     Object.entries(params).forEach((item) => url.searchParams.set(...item));
-    
-    const next = [url.toString()];
-    while (next.length && next[0]) {
-        await fetch(next.pop()).then(r => r.json()).then(json => {
-            if (func(json) && json.links) {
-              next.push(json.links.filter(l => l.rel === 'next').map(l => l.href).pop());
-            }
-        });
-    }
+
+    for (let next = url.toString(); next; next = await fetch(next).then(r => r.json()).then(json => {
+        return (func(json) && json.links) ? json.links.filter(l => l.rel === 'next').map(l => l.href)[0] : 0
+    }));
 }
