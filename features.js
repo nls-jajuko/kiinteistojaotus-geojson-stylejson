70b478fa6b27bce6bba8b17883cb02baacec502b
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-04-08 20:06:08 +0300

    tunes

--- features.js
+++ features.js
@@ -1,17 +1,13 @@
 export async function features(func, endpoint, feat, params) {
     const url = new URL(`${endpoint}/collections/${feat}/items`);
-    url.searchParams.set('limit', 10);
-    Object.entries(params).forEach((item) => {
-        url.searchParams.set(...item)
-    });
-
+    Object.entries(params).forEach((item) => url.searchParams.set(...item));
+    
     const next = [url.toString()];
     while (next.length && next[0]) {
         await fetch(next.pop()).then(r => r.json()).then(json => {
-            if (!func(json) || !json.links) {
-                return;
+            if (func(json) && json.links) {
+              next.push(json.links.filter(l => l.rel === 'next').map(l => l.href).pop());
             }
-            next.push(json.links.filter(l => l.rel === 'next').map(l => l.href).pop());
         });
     }
 }
